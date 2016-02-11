cc3a83247a324b8073a1d3b92d2368dc79ecf8c3
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-11 09:25:32 -0700

    practice with test

--- app/controllers/partial_arts_controller.rb
+++ app/controllers/partial_arts_controller.rb
@@ -1,4 +1,11 @@
 class PartialArts < Sinatra::Base
+
+  helpers do
+    def link_to_artist_show(artist)
+      "<a href='/artists/#{artist.id}'>#{artist.first_name} #{artist.last_name}</a>"
+    end
+  end
+
   get '/' do
     @belt_statistics     = Artist.group("belt").count
     @category_statistics = Artist.group("category").count
